08cebafcd1fe7ec71747d5a3d126637904dc06a4
Rolf Schmidt <rolf.schmidt@znuny.com>
2018-07-31 14:20:19 +0200

    Initial commit 5.0.x.

---
+++ doc/de/feature.md
@@ -0,0 +1,3 @@
+# Znuny4OTRS-AutoCheckbox
+
+Dieses Paket enthält die Funktionalität um Checkboxen von Datumsfeldern automatisch bei Benutzung zu markieren.
---
+++ var/httpd/htdocs/js/Core.Znuny4OTRSAutoCheckbox.js
@@ -0,0 +1,59 @@
+// --
+// Copyright (C) 2012-2018 Znuny GmbH, http://znuny.com/
+// --
+// This software comes with ABSOLUTELY NO WARRANTY. For details, see
+// the enclosed file COPYING for license information (AGPL). If you
+// did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+// --
+
+"use strict";
+
+var Core   = Core || {};
+
+/**
+ * @namespace
+ * @exports TargetNS as Core.Znuny4OTRSAutoCheckbox
+ * @description
+ *      This namespace contains the special functions for Znuny4OTRSAutoCheckbox.
+ */
+Core.Znuny4OTRSAutoCheckbox = (function (TargetNS) {
+
+    TargetNS.Init = function () {
+
+        // find all select elements for date fields
+        $('select').off('change.Z40AutoCheckbox').on('change.Z40AutoCheckbox', function() {
+            TargetNS.AutoCheck(this);
+        });
+
+        // take care about ajax requests so we also need to have this functionality in process management
+        Core.App.Subscribe('Event.AJAX.FunctionCall.Callback', function() {
+            $('select').off('change.Z40AutoCheckbox').on('change.Z40AutoCheckbox', function() {
+                TargetNS.AutoCheck(this);
+            });
+        });
+
+        return true;
+    };
+
+    TargetNS.AutoCheck = function(Element) {
+
+        // get id
+        var ElementID = $(Element).attr('id');
+        if (!ElementID) return;
+
+        // only handle dynamic fields
+        if (!ElementID.match(/^DynamicField/)) return;
+
+        // check which element was changed of the date field
+        var ElementType = ElementID.match(/(Day|Month|Year|Hour|Minute)$/);
+        if (!ElementType) return;
+
+        // mark checkbox for used date field
+        var ElementCheckboxID = ElementID.replace(ElementType[1], 'Used');
+        $('#' + ElementCheckboxID).prop('checked', true);
+    };
+
+    return TargetNS;
+}(Core.Znuny4OTRSAutoCheckbox || {}));
+
+Core.Znuny4OTRSAutoCheckbox.Init();
